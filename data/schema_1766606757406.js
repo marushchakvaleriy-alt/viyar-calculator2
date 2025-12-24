75e48d7239ce11c0affb2711535a97a17511e044
Valeriy <valeriy@example.com>
2025-12-24 22:06:42 +0200

    Auto-update: 2025-12-24 22:06:42

---
+++ data/schema_1766606757406.js
@@ -0,0 +1,41 @@
+const Schema = {
+    "meta": {
+        "version": "2.1",
+        "title": "Новий калькулятор",
+        "lastUpdated": "2025-12-24T20:05:57.406Z"
+    },
+    "processes": [
+        {
+            "id": "pr_setup",
+            "name": "Налаштування",
+            "category": "cat_main"
+        }
+    ],
+    "categories": {
+        "cat_main": {
+            "name": "Основні",
+            "color": "#e0f2fe"
+        }
+    },
+    "groups": [
+        {
+            "id": "g_main",
+            "title": "Загальні параметри"
+        },
+        {
+            "id": "g1766606783227",
+            "title": "іваів"
+        },
+        {
+            "id": "g1766606790202",
+            "title": "іваіваіваіваіваіввввввввввввввввввввв"
+        }
+    ],
+    "fields": [],
+    "products": {
+        "groups": [],
+        "fields": []
+    },
+    "rules": {}
+};
+window.Schema = Schema;
