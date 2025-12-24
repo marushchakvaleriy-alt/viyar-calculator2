a5f0e2be5d13d2106cc09d5aa05010ed3e464c7a
Valeriy <valeriy@example.com>
2025-12-24 23:43:21 +0200

    Auto-update: 2025-12-24 23:43:21

---
+++ data/schema_1766612420075.js
@@ -0,0 +1,33 @@
+const Schema = {
+    "meta": {
+        "version": "2.1",
+        "title": "Новий калькулятор",
+        "lastUpdated": "2025-12-24T21:40:20.075Z"
+    },
+    "processes": [
+        {
+            "id": "pr_setup",
+            "name": "Налаштування",
+            "category": "cat_main"
+        }
+    ],
+    "categories": {
+        "cat_main": {
+            "name": "Основні",
+            "color": "#e0f2fe"
+        }
+    },
+    "groups": [
+        {
+            "id": "g_main",
+            "title": "Загальні параметри"
+        }
+    ],
+    "fields": [],
+    "products": {
+        "groups": [],
+        "fields": []
+    },
+    "rules": {}
+};
+window.Schema = Schema;
