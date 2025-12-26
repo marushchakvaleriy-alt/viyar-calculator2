7f7eef44ad38de231f1a6119a50498cfb554fd3f
VIYAR\v_marushchak <marushchakvaleriy@gmail.com>
2025-12-26 16:39:50 +0200

    initial_sync_on_start

--- data/config_calculators.js
+++ data/config_calculators.js
@@ -5,19 +5,5 @@ window.CalculatorConfig = [
         "icon": "images/кухня.png",
         "file": "data/schema_novyy_kal_kulyator_733.js",
         "desc": "Опис нового калькулятора"
-    },
-    {
-        "id": "schema_re_ys_533",
-        "title": "re[ys",
-        "icon": "images/ліжко.png",
-        "file": "data/schema_re_ys_533.js",
-        "desc": "Опис нового калькулятора"
-    },
-    {
-        "id": "schema_fgdgdfgdfgfgdfgrtret_217",
-        "title": "fgdgdfgdfgfgdfgrtret",
-        "icon": "📦",
-        "file": "data/schema_fgdgdfgdfgfgdfgrtret_217.js",
-        "desc": "Опис нового калькулятора"
     }
 ]; 
