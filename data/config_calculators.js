1282807a41199be9df26a647d02ec72879154e2b
VIYAR\v_marushchak <marushchakvaleriy@gmail.com>
2025-12-26 16:12:06 +0200

    Auto-update: 2025-12-26 16:12:06

--- data/config_calculators.js
+++ data/config_calculators.js
@@ -1,7 +1,7 @@
 window.CalculatorConfig = [
     {
         "id": "schema_novyy_kal_kulyator_733",
-        "title": "Новий калькулятор",
+        "title": "калькулятор",
         "icon": "images/кухня.png",
         "file": "data/schema_novyy_kal_kulyator_733.js",
         "desc": "Опис нового калькулятора"
