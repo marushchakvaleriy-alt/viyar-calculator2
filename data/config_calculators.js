e8844238364aa08b08bb6f2d48172ab8f21b775c
Valeriy <valeriy@example.com>
2026-01-09 22:24:36 +0200

    Auto-save: data/config_calculators.js

--- data/config_calculators.js
+++ data/config_calculators.js
@@ -4,27 +4,6 @@ window.CalculatorConfig = [
         "title": "кухні",
         "icon": "images/кухня.png",
         "file": "data/schema_novyy_kal_kulyator_733.js",
-        "desc": "Опис нового калькулятора"
-    },
-    {
-        "id": "schema_shafy_170",
-        "title": "шафи",
-        "icon": "images/шафа.png",
-        "file": "data/schema_shafy_170.js",
-        "desc": "Опис нового калькулятора"
-    },
-    {
-        "id": "schema_inshi_mebli_526",
-        "title": "інші меблі",
-        "icon": "images/інші меблі.png",
-        "file": "data/schema_inshi_mebli_526.js",
-        "desc": "Опис нового калькулятора"
-    },
-    {
-        "id": "schema_jfgjdfhgj_194",
-        "title": "jfgjdfhgj",
-        "icon": "images/гардеробні системи.png",
-        "file": "data/schema_jfgjdfhgj_194.js",
-        "desc": "Опис нового калькулятора"
+        "desc": "Детальний калькулятор для розрахунку поінтів кухонних меблів з усіма опціями"
     }
 ]; 
