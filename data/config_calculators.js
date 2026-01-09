414f74ab8ee37e005f2311da4f4ce25d79d69a71
Valeriy <valeriy@example.com>
2026-01-09 13:38:00 +0200

    Auto-save: data/config_calculators.js

--- data/config_calculators.js
+++ data/config_calculators.js
@@ -5,12 +5,5 @@ window.CalculatorConfig = [
         "icon": "images/кухня.png",
         "file": "data/schema_novyy_kal_kulyator_733.js",
         "desc": "Опис нового калькулятора"
-    },
-    {
-        "id": "schema_iuluililuiyoly_304",
-        "title": "tryr",
-        "icon": "📦",
-        "file": "data/schema_iuluililuiyoly_304.js",
-        "desc": "Опис нового калькулятора"
     }
 ]; 
