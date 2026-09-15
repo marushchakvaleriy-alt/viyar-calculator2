9ea02e6c2564e65cc6bc7d7f70fb1aa574b5f7ef
Марущак Валерій <v_marushchak@viyar.com.ua>
2026-09-15 16:32:21 +0300

    Auto-save: data/config_calculators.js

--- data/config_calculators.js
+++ data/config_calculators.js
@@ -5,7 +5,7 @@ window.CalculatorConfig = [
         "icon": "🛠️",
         "desc": "Кухонне наповнення, напрямні, завіси, кріплення, гардеробне наповнення, стенд розпродажу",
         "isCategory": true,
-        "badge": "2 калькуляторів",
+        "badge": "1 калькуляторів",
         "items": [
             {
                 "id": "schema_stand_kuhonne_napovnennya",
@@ -13,14 +13,6 @@ window.CalculatorConfig = [
                 "icon": "🗄️",
                 "file": "data/schema_stand_kuhonne_napovnennya.js",
                 "desc": "Карго, сушки, підіймачі, органайзери, сміттєві системи",
-                "isPlaceholder": false
-            },
-            {
-                "id": "schema_stand_napryamni_zavisy",
-                "title": "Зона напрямних, завіс та підіймачів",
-                "icon": "🔧",
-                "file": "data/schema_stand_napryamni_zavisy.js",
-                "desc": "Стенди систем висування, петель, підіймальних механізмів",
                 "isPlaceholder": false
             }
         ]
