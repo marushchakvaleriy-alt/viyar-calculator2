4581b5699e2549cf474e6dd2286bc6f115f9214a
Марущак Валерій <v_marushchak@viyar.com.ua>
2026-09-16 10:06:15 +0300

    Update Functional Hardware calculator, fastener modal, and designer fix

--- data/config_calculators.js
+++ data/config_calculators.js
@@ -1,25 +1,9 @@
 window.CalculatorConfig = [
     {
-        "id": "schema_stand_skladski_mebli",
-        "title": "Складські меблі",
-        "icon": "📦",
-        "file": "data/schema_stand_skladski_mebli.js",
-        "desc": "Стелажі, столи пакування, комірники",
-        "isPlaceholder": true
-    },
-    {
-        "id": "schema_stand_kuhonni_komplektuyuchi",
-        "title": "Кухонні комплектуючі та техніка",
-        "icon": "🍳",
-        "file": "data/schema_stand_kuhonni_komplektuyuchi.js",
-        "desc": "Мийки, змішувачі, техніка, витяжки",
-        "isPlaceholder": true
-    },
-    {
         "id": "schema_funktsional_na_furnitura_564",
         "title": "Функціональна фурнітура",
         "icon": "📦",
         "file": "data/schema_funktsional_na_furnitura_564.js",
         "desc": "Опис нового калькулятора"
     }
-]; 
+];
